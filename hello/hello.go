cf11d58c042dd7a6479642ff0ca56312cb4f4789
T-Oner <1@fxcdev.com>
2020-12-18 14:18:14 +0800

    Add multiple Persion

--- hello/hello.go
+++ hello/hello.go
@@ -12,11 +12,20 @@ func main() {
 	log.SetPrefix("greetings: ")
 	log.SetFlags(0)
 
-	message, err := greetings.Hello("Toner")
+	names := []string{
+		"Toner",
+		"Emrys",
+		"Frank",
+	}
+
+	messages, err := greetings.Hellos(names)
+
+	// message, err := greetings.Hello("Toner")
 
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	fmt.Println(message)
+	fmt.Println(messages)
+	// fmt.Println(message)
 }
